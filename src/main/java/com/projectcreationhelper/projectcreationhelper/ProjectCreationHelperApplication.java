43a4052e04c2396c40a32a663f38f98d4b9abdd2
johnburitto <79087305+johnburitto@users.noreply.github.com>
2024-05-27 20:51:31 +0300

    Init

---
+++ src/main/java/com/projectcreationhelper/projectcreationhelper/ProjectCreationHelperApplication.java
@@ -0,0 +1,11 @@
+package com.projectcreationhelper.projectcreationhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCreationHelperApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCreationHelperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/projectcreationhelper/projectcreationhelper/ProjectCreationHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectcreationhelper.projectcreationhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCreationHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
